74114207e5e6e850d89b7ea077796770d2e74e70
韩啸 <hxverygood@hotmail.com>
2017-11-14 18:15:48 +0800

    MOD: 修改细节

--- Config/HSConfig.h
+++ Config/HSConfig.h
@@ -8,84 +8,9 @@
 
 #import <Foundation/Foundation.h>
 
-FOUNDATION_EXPORT NSString *const kAPIBaseURL;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL1;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL7;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL8;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL2;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL3;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL4;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL5;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL6;
-FOUNDATION_EXPORT NSString *const kAPIBaseURL9;
-FOUNDATION_EXPORT NSString *const kAPIBaseURLzs;
-FOUNDATION_EXPORT NSString *const kAPIBaseURLSMRZ;
-FOUNDATION_EXPORT NSString *const ScanIdCardMessage;
-FOUNDATION_EXPORT NSString *const kAPIBaseURScan;
 FOUNDATION_EXPORT NSString *const kAppStoreUrlStr;
 FOUNDATION_EXPORT NSString *const kSystemPreferenceContact;
 FOUNDATION_EXPORT NSInteger const authCodeQuantity;
-
-
-//mainApiName
-FOUNDATION_EXPORT NSString *const mainApiName;
-
-//mainApiName
-FOUNDATION_EXPORT NSString *const mainApiName_WEB;
-
-
-// 认证类型
-typedef NS_ENUM(NSInteger, AuthSuccessType) {
-    AuthSuccessTypePersonMail,
-    AuthSuccessTypeBusinessMail,
-    AuthSuccessTypeZhima,
-    AuthSuccessTypeTaobao,
-    AuthSuccessTypeCreditCard
-};
-
-// 借款详情类型
-typedef NS_ENUM(NSUInteger, BorrowDetailType) {
-    BorrowDetailTypeOrigin,         // 用于原始的借款详情页面
-    BorrowDetailTypeForSigning,     // 用于待签约界面
-    BorrowDetailTypeForRepayment    // 用于还款界面
-};
-
-/// 信用卡登录类型
-typedef NS_ENUM(NSInteger, CreditCardLoginType) {
-    CreditCardLoginTypeUserName,
-    CreditCardLoginTypeUserNameAndCodeAndDynamicPassword
-};
-
-// 申请记录类型
-typedef NS_ENUM(NSInteger, QuotaManagementType) {
-    QuotaManagementTypeAll,
-    QuotaManagementTypeStore
-};
-
-/// 认证类型（产品1-4）
-typedef NS_ENUM(NSInteger, AuthTipOption) {
-    AuthTipOptionRealName,
-    AuthTipOptionRequired,
-    AuthTipOptionProduct1,
-    AuthTipOptionProduct2,
-    AuthTipOptionProduct3,
-    AuthTipOptionProduct4
-};
-
-/// 产品类型（线上产品1-4，线下产品1-4）
-typedef NS_ENUM(NSInteger, ProductOption) {
-    ProductOption1,
-    ProductOption2,
-    ProductOption3,
-    ProductOption4,
-};
-
-typedef NS_ENUM(NSInteger, OfflineProductOption) {
-    OfflineProductOption1,
-    OfflineProductOption2,
-    OfflineProductOption3,
-    OfflineProductOption4
-};
 
 
 /// 认证状态（未完成、认证中、已认证、认证失败、社保公积金没有全部完成）
--- Config/HSConfig.m
+++ Config/HSConfig.m
@@ -11,107 +11,23 @@
 NSInteger const authCodeQuantity = 6;
 
 #warning fix: url of app in AppStore
-NSString *const kAppStoreUrlStr = @"https://itunes.apple.com/WebObjects/MZStore.woa/wa/viewSoftware?id=1281895479";
+NSString *const kAppStoreUrlStr = @"";
 NSString *const kSystemPreferenceContact = @"Privacy&path=CONTACTS";
 
 #ifdef DEBUG
 
 #pragma mark - 本地
-// 朱洪郑
-NSString *const kAPIBaseURL = @"http://192.168.3.210:8080";
-// 栋梁192.168.3.39
-NSString *const kAPIBaseURL1 = @"http://192.168.3.19:8080";
-//NSString *const kAPIBaseURL1 = @"http://192.168.3.39:8080";
-// 刘帅
-NSString *const kAPIBaseURL7 = @"http://192.168.3.14:8080";
-// 朱洪郑
-NSString *const kAPIBaseURL2 = @"http://192.168.3.210:8080";
-// 路党伟
-NSString *const kAPIBaseURL4 = @"http://192.168.3.37:8080";
-// 崔永娟
-NSString *const kAPIBaseURL8 = @"http://192.168.3.38:8080";
-// 浩敏
-NSString *const kAPIBaseURL3 = @"http://192.168.3.16:8089";
-// 刘斌
-NSString *const kAPIBaseURL5 = @"http://192.168.3.210:8080";
-// 招商银行1
-NSString *const kAPIBaseURLzs = @"http://192.168.2.117:8066";
-// 扫二维码信息
-NSString *const kAPIBaseURScan = @"http://113.200.203.114:8070";
-// 社保公积金
-NSString *const kAPIBaseURL6 = @"http://192.168.3.19:8080";
 
-NSString *const mainApiName = @"hsfs-web/web";
-NSString *const mainApiName_WEB = @"hsfs-web";
-// 第3方运营商认证
-NSString *const kAPIBaseURL9 = @"http://192.168.3.4:8081";
 
 
 
 #pragma mark - 外网
-//// 朱洪郑
-//NSString *const kAPIBaseURL = @"http://113.200.105.34:8086";
-//// 栋梁
-//NSString *const kAPIBaseURL1 = @"http://113.200.105.35:8085";
-//// 刘帅
-//NSString *const kAPIBaseURL7 = @"http://113.200.105.35:8085";
-//// 朱洪郑
-//NSString *const kAPIBaseURL2 = @"http://113.200.105.34:8086";
-//// 路党伟
-//NSString *const kAPIBaseURL4 = @"http://113.200.105.34:8086";
-//// 崔永娟
-//NSString *const kAPIBaseURL8 = @"http://113.200.105.34:8086";
-//// 浩敏
-//NSString *const kAPIBaseURL3 = @"http://113.200.105.37:8080";
-//// 刘斌
-//NSString *const kAPIBaseURL5 = @"http://113.200.105.34:8086";
-//// 招商银行
-//NSString *const kAPIBaseURLzs = @"http://113.200.105.34:8086";
-//// 实名认证
-//NSString *const kAPIBaseURScan = @"http://113.200.105.35:8085";
-//
-//// 社保公积金
-//NSString *const kAPIBaseURL6 = @"http://113.200.105.35:8085";
-//
-//NSString *const mainApiName = @"web";
-//NSString *const mainApiName_WEB = @"";
-
 
 
 #else
-// 朱洪郑
-NSString *const kAPIBaseURL = @"http://113.200.105.34:8086";
-// 栋梁
-NSString *const kAPIBaseURL1 = @"http://113.200.105.35:8085";
-// 刘帅
-NSString *const kAPIBaseURL7 = @"http://113.200.105.35:8085";
-// 朱洪郑
-NSString *const kAPIBaseURL2 = @"http://113.200.105.34:8086";
-// 路党伟
-NSString *const kAPIBaseURL4 = @"http://113.200.105.34:8086";
-// 崔永娟
-NSString *const kAPIBaseURL8 = @"http://113.200.105.34:8086";
-// 浩敏
-NSString *const kAPIBaseURL3 = @"http://113.200.105.37:8080";
-// 刘斌
-NSString *const kAPIBaseURL5 = @"http://113.200.105.34:8086";
 
-// 招商银行
-NSString *const kAPIBaseURLzs = @"http://113.200.105.34:8086";
-// 实名认证
-NSString *const kAPIBaseURScan = @"http://113.200.105.35:8085";
-// 社保公积金
-NSString *const kAPIBaseURL6 = @"http://113.200.105.35:8085";
-//// 第3方运营商认证
-//NSString *const kAPIBaseURL9 = @"http://192.168.3.4:8081";
-
-NSString *const mainApiName = @"web";
-NSString *const mainApiName_WEB = @"";
 
 #endif
-
-//192.168.3.19:8080/hsfs-web/web/youdun/getvalue.do
-NSString *const ScanIdCardMessage = @"http://124.89.33.70:8191/web/youdun/getvalue";
 
 
 
