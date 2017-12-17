f7bc9fd8c56419e0e601f97e935a6fd1cd0a1401
韩啸 <hxverygood@hotmail.com>
2017-12-17 12:15:42 +0800

    ADD: 多列数据pickerView

---
+++ Public/View/HSMutiColPickerView.h
@@ -0,0 +1,26 @@
+//
+//  HSMutiColPickerView.h
+//  HSRongyiBao
+//
+//  Created by hoomsun on 2017/12/12.
+//  Copyright © 2017年 hoomsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HSMutiColPickerView : UIView
+
+@property (nonatomic, strong) UIPickerView *pickerView;
+@property (nonatomic, strong) UIColor *buttonColor;
+@property (nonatomic, strong) NSArray<NSArray<NSString *> *> *dataArrays;
+@property (nonatomic, assign, readonly) BOOL isVisable;
+
+@property (nonatomic, copy) void (^selectRowBlock)(NSInteger component, NSInteger row);
+@property (nonatomic, copy) void (^confirmButtonAction)(NSInteger componentCount, NSArray *rowArray);
+
+- (instancetype)initWithDataArrays:(NSArray<NSArray *> *)dataArrays;
+
+- (void)show;
+- (void)dismiss;
+
+@end
